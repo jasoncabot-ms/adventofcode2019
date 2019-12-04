d411b0d777bb44d178bb7d3d65922d1910b33615
Jason Cabot <Jason.Cabot@microsoft.com>
2019-12-04 21:07:03 +0000

    Day 3 - part 2
    Converted algorithm to perform a breadth-first search of all possible
    contact points on all wires and sum the distance
    
    We maintain the closest seen value
    
    Not the most efficient solution but also not the worst

--- advent3/src/main.rs
+++ advent3/src/main.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 use std::collections::HashSet;
+use std::iter::FromIterator;
 use std::env;
 
 fn main() {
@@ -11,38 +12,48 @@ fn main() {
     let reader = BufReader::new(input);
 
     // Build a list of unique co-ordinates for each represented wire
-    let mut wires: Vec<HashSet<(isize, isize)>> = Vec::new();
+    let mut wires: Vec<Vec<(isize, isize)>> = Vec::new();
     for line in reader.lines() {
-        let mut wire = HashSet::new();
+        let mut wire = Vec::new();
         let mut position = (0, 0);
         for path in line.expect("read wire").split(",").map(to_path) {
             for point in path.all_points(&mut position) {
-                wire.insert(point);
+                wire.push(point);
             }
         }
         wires.push(wire);
     }
 
     // Check for co-ordinates that appear for all wires
-    let mut contact_points: HashSet<_> = wires.pop().unwrap();
-    while !wires.is_empty() {
-        let next_contact_points: HashSet<_> = wires.pop().unwrap();
-        contact_points = contact_points.intersection(&next_contact_points).cloned().collect();
-    }
-    println!("contact points {:?}", contact_points);
-
-    // Find the point closest to 0, 0
-    // ideally we should maintain a heap whilst finding the intersection but meh
-    let mut closest = (isize::max_value(), (0, 0));
-    for point in contact_points.iter() {
-        let distance = point.0.abs() + point.1.abs(); // manhattan distance
-        if distance < closest.0 {
-            closest.0 = distance;
-            closest.1 = *point;
+    let mut contact_points: HashSet<(isize, isize)> = HashSet::new();
+    for points in wires.clone() {
+        let set: HashSet<(isize, isize)> = HashSet::from_iter(points);
+        if contact_points.is_empty() {
+            contact_points = set;
+        } else {
+            contact_points = contact_points.intersection(&set).cloned().collect();
         }
-        // we can ignore the fact that there may exist multiple contact points the same distance away
     }
-    println!("point: {:?}, distance: {}", closest.1, closest.0);
+
+    // For each contact point, count the total number of steps for all wires
+    // Since we are finding the minimal number of steps, we must consider every
+    // contact point rather than just picking the first we encounter on a wire
+    // due to weird edge cases
+    let mut closest = (usize::max_value(), (0, 0));
+    for point in contact_points {
+        let mut total_steps = 0;
+        for wire in wires.clone() {
+            let steps = wire.iter().position(|p| *p == point).unwrap() + 1;
+            total_steps += steps;
+        }
+        println!("Contact point {:?} is {:?} steps away", point, total_steps);
+        if total_steps < closest.0 {
+            closest.0 = total_steps;
+            closest.1 = point
+        }
+    }
+    println!("Done");
+    println!("Contact point {:?} is {:?} steps away", closest.1, closest.0);
 }
 
 #[derive(Debug)]
@@ -52,8 +63,8 @@ struct Path {
 }
 
 impl Path {
-    fn all_points(&self, position: &mut (isize, isize)) -> HashSet<(isize, isize)> {
-        let mut nodes = HashSet::new();
+    fn all_points(&self, position: &mut (isize, isize)) -> Vec<(isize, isize)> {
+        let mut nodes = Vec::new();
         for _ in 0..self.length {
             match self.direction {
                 'R' => position.0 += 1,
@@ -62,7 +73,7 @@ impl Path {
                 'D' => position.1 += 1,
                 _ => panic!("unknown direction")
             };
-            nodes.insert(position.clone());
+            nodes.push(position.clone());
         }
         return nodes;
     }
